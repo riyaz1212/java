27ab5656a00fcbf5f8752af78ab6015c08c30f35
riyaz1212 <riyaz.coding@gmail.com>
2023-12-27 06:18:46 -0600

    Java Operators

---
+++ java/src/javaOperators/JavaOperators.java
@@ -0,0 +1,15 @@
+package javaOperators;
+
+public class JavaOperators {
+
+	public static void main(String[] args) {
+int sum1 =  100 + 50;
+int sum2 = sum1 + 250;
+int sum3 = sum2 + sum1;
+
+System.out.println(sum1);
+System.out.println(sum2);
+System.out.println(sum3);
+	}
+
+}
