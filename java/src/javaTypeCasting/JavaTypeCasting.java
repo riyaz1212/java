c62662cd17c5a99ba885cb18134de80d3b90c617
riyaz1212 <riyaz.coding@gmail.com>
2023-12-27 06:11:08 -0600

    Added automatic casting

---
+++ java/src/javaTypeCasting/JavaTypeCasting.java
@@ -0,0 +1,13 @@
+package javaTypeCasting;
+
+public class JavaTypeCasting {
+
+	public static void main(String[] args) {
+int myInt =9;
+double myDouble = myInt;
+
+System.out.println(myInt);
+System.out.println(myDouble);
+	}
+
+}
