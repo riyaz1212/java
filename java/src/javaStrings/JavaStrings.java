df09d253e3b1488b8c7e3312e011993d3a900bd2
riyaz1212 <riyaz.coding@gmail.com>
2023-12-27 06:32:26 -0600

    Java Strings

---
+++ java/src/javaStrings/JavaStrings.java
@@ -0,0 +1,15 @@
+package javaStrings;
+
+public class JavaStrings {
+
+	public static void main(String[] args) {
+String greeting = "Hello!";
+System.out.println(greeting.toUpperCase());
+System.out.println(greeting.toLowerCase());
+System.out.println(greeting.indexOf(greeting));
+System.out.println(greeting.length());
+System.out.println(greeting.indexOf(0));
+
+	}
+
+}
